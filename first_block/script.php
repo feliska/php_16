9322673e86701f5032e1be70f7635127b8c8170b
Feliska <feliska@me.com>
2017-09-25 21:25:17 +0200

    Доработка задания. Добавление проверки введенных символов. Изменение цикла while.

--- first_block/script.php
+++ first_block/script.php
@@ -4,19 +4,26 @@ $user_input = $_GET['x'];
 $first = 1;
 $second = 1;
 
-echo "Вы ввели число ", $user_input, '<br>';
-
-while ($user_input) {
-    if ($first > $user_input) {
-        echo "Задуманное число НЕ входит в числовой ряд";
+switch (is_numeric($user_input)) {
+    case 1:
+        echo "Вы ввели число ", $user_input, '<br>';
+        if ($first > $user_input) {
+            echo "Задуманное число НЕ входит в числовой ряд";
+            break;
+        } elseif ($first == $user_input) {
+            echo "Задуманное число входит в числовой ряд";
+            break;
+        } else {
+            while ($first < $user_input) {
+                $third = $first;
+                $first += $second;
+                $second = $third;
+                echo $first, '<br>';
+            }
+        }
+        echo '<br>', "Это числа Фибоначчи :))";
         break;
-    } elseif ($first == $user_input) {
-        echo "Задуманное число входит в числовой ряд";
+    case 0:
+        echo  "<b>Для работы программы вам необходимо ввести число!<b/>";
         break;
-    } else {
-        $third = $first;
-        $first += $second;
-        $second = $third;
-        echo $first, '<br>';
-    }
 }
