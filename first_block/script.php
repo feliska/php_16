546d5c9dfaf270dc387a8c2f549d0c3403e98cec
Feliska <feliska@me.com>
2017-09-25 21:54:58 +0200

    Избавление от Notice, когда ничего не введено

--- first_block/script.php
+++ first_block/script.php
@@ -1,29 +1,34 @@
 <?php
 
-$user_input = $_GET['x'];
 $first = 1;
 $second = 1;
 
-switch (is_numeric($user_input)) {
-    case 1:
-        echo "Вы ввели число ", $user_input, '<br>';
-        if ($first > $user_input) {
-            echo "Задуманное число НЕ входит в числовой ряд";
-            break;
-        } elseif ($first == $user_input) {
-            echo "Задуманное число входит в числовой ряд";
-            break;
-        } else {
-            while ($first < $user_input) {
-                $third = $first;
-                $first += $second;
-                $second = $third;
-                echo $first, '<br>';
+if (isset($_GET['x'])) {
+    $user_input = $_GET['x'];
+    switch (is_numeric($user_input)) {
+        case 1:
+            echo "Вы ввели число ", $user_input, '<br>';
+            if ($first > $user_input) {
+                echo "Задуманное число НЕ входит в числовой ряд";
+                break;
+            } elseif ($first == $user_input) {
+                echo "Задуманное число входит в числовой ряд";
+                break;
+            } else {
+                while ($first < $user_input) {
+                    $third = $first;
+                    $first += $second;
+                    $second = $third;
+                    echo $first, '<br>';
+                }
             }
-        }
-        echo '<br>', "Это числа Фибоначчи :))";
-        break;
-    case 0:
-        echo  "<b>Для работы программы вам необходимо ввести число!<b/>";
-        break;
+            echo '<br>', "Это числа Фибоначчи :))";
+            break;
+        case 0:
+            echo  "<b>Для работы программы вам необходимо ввести число!<b/>";
+            break;
+    }
+} else {
+    echo "<b>Вы ничего не ввели :(( Попробуйте еще раз.<b/>";
 }
+
