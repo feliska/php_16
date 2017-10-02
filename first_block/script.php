576d472f3188b193ead6516f03d8bc84c45b766e
Feliska <feliska@me.com>
2017-10-03 00:54:12 +0200

    Изменение логики вывода сообщений о введенном числе.

--- first_block/script.php
+++ first_block/script.php
@@ -5,28 +5,21 @@ $second = 1;
 
 if (isset($_GET['x'])) {
     $user_input = $_GET['x'];
-    switch (is_numeric($user_input)) {
-        case 1:
-            echo "Вы ввели число ", $user_input, '<br>';
-            if ($first > $user_input) {
-                echo "Задуманное число НЕ входит в числовой ряд";
-                break;
-            } elseif ($first == $user_input) {
-                echo "Задуманное число входит в числовой ряд";
-                break;
-            } else {
-                while ($first < $user_input) {
-                    $third = $first;
-                    $first += $second;
-                    $second = $third;
-                    echo $first, '<br>';
-                }
-            }
-            echo '<br>', "Это числа Фибоначчи :))";
-            break;
-        case 0:
-            echo  "<b>Для работы программы вам необходимо ввести число!<b/>";
-            break;
+    if (is_numeric($user_input)) {
+        echo "Вы ввели число ", $user_input, '<br>';
+        while ($first < $user_input) {
+            $third = $first;
+            $first += $second;
+            $second = $third;
+            echo $first, '<br>';
+        }
+        if ($first == $user_input) {
+            echo "Задуманное число входит в числовой ряд";
+        } else {
+            echo "Задуманное число НЕ входит в числовой ряд";
+        }
+    }  else {
+        echo  "<b>Для работы программы вам необходимо ввести число!<b/>";
     }
 } else {
     echo "<b>Вы ничего не ввели :(( Попробуйте еще раз.<b/>";
