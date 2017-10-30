118fc77bee4d1b1446331abe6a06be7feb1b28b6
Yana <feliska@me.com>
2017-10-30 19:20:20 +0200

    Урок 4.1. Реляционные базы данных и SQL.

---
+++ fourth_block/first_lesson.php
@@ -0,0 +1,56 @@
+<?php
+
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=global", "root", "root");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Подключение не удалось: ' . $e->getMessage();
+}
+
+$sql = "SELECT * FROM books ORDER BY name";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Библиотека</title>
+    <meta charset="UTF-8">
+    <style>
+        table {
+            border-spacing: 0;
+            border-collapse: collapse;
+        }
+
+        table td, table th {
+            border: 1px solid #ccc;
+            padding: 5px;
+        }
+
+        table th {
+            background: #eee;
+        }
+    </style>
+</head>
+<body>
+<h1>Список книг</h1>
+<table>
+    <tr>
+        <th>Название</th>
+        <th>Автор</th>
+        <th>Жанр</th>
+        <th>Год выпуска</th>
+        <th>ISBN</th>
+    </tr>
+    <tr>
+        <? foreach ($pdo->query($sql) as $row) : ?>
+        <td><?= $name = $row['name']; ?></td>
+        <td><?= $author = $row['author']; ?></td>
+        <td><?= $genre = $row['genre']; ?></td>
+        <td><?= $year = $row['year']; ?></td>
+        <td><?= $isbn = $row['isbn']; ?></td>
+    </tr>
+    <? endforeach; ?>
+</table>
+</body>
+</html>
